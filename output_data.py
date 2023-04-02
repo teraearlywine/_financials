dfc7534b735fa8b3f5ad0314cb892206a970ccc8
Tera Earlywine <teraearlywine22@gmail.com>
2023-04-02 15:27:18 -0700

    *

--- output_data.py
+++ output_data.py
@@ -1,3 +1,11 @@
-
 import sql_utils
-sql_utils.execute_sql('SELECT 1')
+import sql 
+
+def output_data(filepath):
+    # Read sql file, add to sql_content variable
+    with open(filepath, 'r') as sql_file:
+        sql_content = sql_file.read()
+    # Using sql utils, execute sql_content variable
+    sql_utils.execute_sql(sql_content)
+
+# read_sql_filename('sql/sample.sql')
--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -1,9 +1,6 @@
 import parse_config as pc 
 import utils
 
-# sql = "%s"
-# sql = "SELECT 1"
-
 connection = pc.pymysql.connect(
     host = pc.db_host,
     port = pc.db_port,
@@ -25,37 +22,3 @@ def commit_sql(sql_string):
     m_cursor = connection.cursor() 
     m_cursor.execute(sql_string)
     connection.commit()
-
-# with open('example.sql', 'r') as sql_file:
-#     sql_content = sql_file.read()
-
-# # Split the SQL statements into a list
-# sql_statements = sql_content.split(';')
-
-# # Execute the SQL statements
-# with connection.cursor() as cursor:
-#     for statement in sql_statements:
-#         # Strip any leading/trailing whitespaces and check if the statement is not empty
-#         if statement.strip():
-#             cursor.execute(statement)
-#             connection.commit()
-
-
-# def executeScriptsFromFile(filename):
-#     # Open and read the file as a single buffer
-#     fd = open(filename, 'r')
-#     sqlFile = fd.read()
-#     fd.close()
-
-#     # all SQL commands (split on ';')
-#     sqlCommands = sqlFile.split(';')
-
-#     # Execute every command from the input file
-#     for command in sqlCommands:
-#         # This will skip and report errors
-#         # For example, if the tables do not yet exist, this will skip over
-#         # the DROP TABLE commands
-#         try:
-#             c.execute(command)
-#         except OperationalError, msg:
-#             print("Command skipped: ", msg)
